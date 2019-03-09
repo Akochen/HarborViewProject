d227871746070a290075e6c4b81f96e77a22cc55
Adam Kochen <akochen74@gmail.com>
2019-03-09 17:48:26 -0500

    Add project files.

---
+++ WebApplication3/Models/StudentDbConnectionClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+
+    public class StudentDbConnectionClass
+    {
+        public static String selectStudents(String queryString)
+        {
+            String cString = System.Configuration.ConfigurationManager.ConnectionStrings["DBConnect"].ConnectionString;
+            String html = "<ul>";
+
+            using (SqlConnection connection = new SqlConnection(cString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                connection.Open();
+                using ( var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        html += "<li> Name: " + reader.GetString(0) + " " + reader.GetString(1) + " </li>";
+                    }
+                }
+                connection.Close();
+            }
+            html += "</ul>";
+            return html;
+        }
+    }
+
+}
---
+++ WebApplication3/Models/UserDbConnectionClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class UserDbConnectionClass
+    {
+        public static String login(String queryString)
+        {
+            String cString = System.Configuration.ConfigurationManager.ConnectionStrings["DBConnect"].ConnectionString;
+            String html = "<ul>";
+
+            using (SqlConnection connection = new SqlConnection(cString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    String output = reader.GetString(0);
+                    connection.Close();
+                    return output;
+                } else
+                {
+                    connection.Close();
+                    return "Error: Incorrect username or password.";
+                }
+            }
+        }
+    }
+}
