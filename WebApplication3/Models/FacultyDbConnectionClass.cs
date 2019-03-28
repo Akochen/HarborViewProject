5e6bad7631b95efa0bb7353400b42b74acf37b4e
Adam Kochen <AKOCHEN74@gmail.com>
2019-03-28 04:43:47 -0400

    Some faculty stuff

---
+++ WebApplication3/Models/FacultyDbConnectionClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using WebApplication3.HelperClasses;
+
+namespace WebApplication3.Models
+{
+    public class FacultyDbConnectionClass
+    {
+        public static List<Section> viewSchedule(String userID, String semester, String year)
+        {
+            List<Section> sections = new List<Section>();
+            String cString = System.Configuration.ConfigurationManager.ConnectionStrings["DBConnect"].ConnectionString;
+            String queryString = "SELECT * FROM [HarborViewUniversity].[dbo].[faculty_schedule_view] WHERE faculty_id = " + userID + " AND semster = '" + semester + "' AND [year] = '" + year + "'";
+            using (SqlConnection connection = new SqlConnection(cString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                connection.Open();
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        sections.Add(new Section(reader.GetString(1), reader.GetString(2), reader.GetString(3), reader.GetString(4), reader.GetString(5), reader.GetString(6)
+                            , reader.GetString(7), reader.GetInt32(8)));
+                    }
+                }
+                connection.Close();
+            }
+
+            return sections;
+        }
+        
+    }
+}
