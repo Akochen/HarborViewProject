e61e49471e967c976e2973fd0e021374f5bb7e8d
Adam Kochen <akochen74@gmail.com>
2019-04-13 16:55:25 -0400

    Added search master schedule for administrator

--- WebApplication3/Models/AdminDbConnectionClass.cs
+++ WebApplication3/Models/AdminDbConnectionClass.cs
@@ -30,5 +30,86 @@ namespace WebApplication3.Models
             }
             return departments;
         }
+
+        public static EnrollmentSemesterHelper createViewScheduleHelper()
+        {
+            String cString = System.Configuration.ConfigurationManager.ConnectionStrings["DBConnect"].ConnectionString;
+            String deptString = "SELECT [department_full_name] FROM [HarborViewUniversity].[dbo].[department]";
+            String dayString = "SELECT DISTINCT [days] FROM [HarborViewUniversity].[dbo].[section_view]";
+            String timeString = "SELECT DISTINCT FORMAT(CAST([start_time] AS datetime), 'h:mm tt') AS start_time, FORMAT(CAST([end_time] AS datetime), 'h:mm tt') AS end_time, CAST([end_time] AS datetime) AS order_time FROM [HarborViewUniversity].[dbo].[time_slot] ORDER BY order_time";
+            List<String> departments = new List<string>();
+            List<String> days = new List<string>();
+            List<String> times = new List<string>();
+            List<String> semesters = new List<string>();
+            //Grab semester and year selector data
+            semesters.Add(SemesterDataHelper.getSemesterSeason() + " " + SemesterDataHelper.getSemesterYear());
+            semesters.Add(SemesterDataHelper.getNextSemesterSeason() + " " + SemesterDataHelper.getNextSemesterYear());
+
+            using (SqlConnection connection = new SqlConnection(cString))
+            {
+                SqlCommand command1 = new SqlCommand(deptString, connection);
+                connection.Open();
+                using (var reader = command1.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        departments.Add(reader.GetString(0));
+                    }
+                }
+
+                SqlCommand command2 = new SqlCommand(dayString, connection);
+                using (var reader = command2.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        days.Add(reader.GetString(0));
+                    }
+                }
+
+                SqlCommand command3 = new SqlCommand(timeString, connection);
+                using (var reader = command3.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        times.Add(reader.GetString(0));
+                    }
+                }
+
+                connection.Close();
+            }
+
+            return new EnrollmentSemesterHelper(departments, times, days, semesters);
+        }
+
+        public static List<Section> searchSections(String searchYear, String searchSemester, String instructor, String days, String time, String courseID, String courseName, String department)
+        {
+            List<Section> sections = new List<Section>();
+            String cString = System.Configuration.ConfigurationManager.ConnectionStrings["DBConnect"].ConnectionString;
+            String queryString = "SELECT [section_id],[course_id],[course_name],[instructor],[days],[start_time],[end_time],[semster],[year],[building_full_name],[room_number],[type],[capactiy],[seats_remaining] FROM [HarborViewUniversity].[dbo].[section_view] " +
+                "WHERE  [year] = '" + searchYear + "' AND semster = '" + searchSemester + "' AND [instructor] LIKE '%" + instructor + "%' AND [days] = '" + days + "' AND [start_time] LIKE '%" + time + "%' " +
+                "AND [course_id] LIKE '%" + courseID + "%' AND [course_name] LIKE '%" + courseName + "%' AND [department_full_name] LIKE '%" + department + "%'  ORDER BY course_id";
+            if (days.Equals(""))
+            {
+                queryString = "SELECT [section_id],[course_id],[course_name],[instructor],[days],[start_time],[end_time],[semster],[year],[building_full_name],[room_number],[type],[capactiy],[seats_remaining] FROM [HarborViewUniversity].[dbo].[section_view] " +
+                "WHERE  [year] = '" + searchYear + "' AND semster = '" + searchSemester + "' AND [instructor] LIKE '%" + instructor + "%' AND [start_time] LIKE '%" + time + "%' " +
+                "AND [course_id] LIKE '%" + courseID + "%' AND [course_name] LIKE '%" + courseName + "%' AND [department_full_name] LIKE '%" + department + "%'  ORDER BY course_id";
+            }
+            using (SqlConnection connection = new SqlConnection(cString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                connection.Open();
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        sections.Add(new Section(reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetString(3), reader.GetString(4), reader.GetString(5), reader.GetString(6), reader.GetString(7)
+                            , reader.GetString(8), reader.GetString(9), reader.GetInt32(10), reader.GetString(11), reader.GetByte(12), reader.GetByte(13)));
+                    }
+                }
+                connection.Close();
+            }
+
+            return sections;
+        }
     }
 }
