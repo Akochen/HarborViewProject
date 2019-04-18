948d3de4272181652a740ea7d1c04815735db2c7
Adam Kochen <akochen74@gmail.com>
2019-04-18 19:12:21 -0400

    Started degree audit

---
+++ WebApplication3/HelperClasses/DegreeAuditMajorReqs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.HelperClasses
+{
+    public class DegreeAuditMajorReqs
+    {
+        public String courseID;
+        public String courseName;
+        public String prereqsToTake;
+        public String isComplete;
+
+        public DegreeAuditMajorReqs(string courseID, string courseName, string prereqsToTake, string isComplete)
+        {
+            this.courseID = courseID;
+            this.courseName = courseName;
+            this.prereqsToTake = prereqsToTake;
+            this.isComplete = isComplete;
+        }
+    }
+}
